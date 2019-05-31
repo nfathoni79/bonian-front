39d7ada5ace4e5d227c5f84be641877f2b680cb6
User <thinktobad@gmail.com>
2019-05-31 16:27:01 +0700

    application

--- webroot/js/themejs/application.js
+++ webroot/js/themejs/application.js
@@ -672,7 +672,7 @@ $(document).ready(function() {
 
 
     $('.waShare').on('click',function(){
-        window.open('https://api.whatsapp.com/send?text=Beli '+$(this).data('title')+' '+$(this).data('url')+'/'+reff+'/wa Harga Promo Rp. '+numeral($(this).data('price')).format('0,0')+' hanya di Bonian! Dapatkan Bonus Point '+numeral($(this).data('point')).format('0,0')+' serta dapatkan Potongan Kupon dan Voucher Diskon, Beli Sekarang ! ','_blank','width=600, height=368');
+        window.open('https://api.whatsapp.com/send?text=Beli '+$(this).data('title')+' '+$(this).data('url')+'/wa/'+reff+' Harga Promo Rp. '+numeral($(this).data('price')).format('0,0')+' hanya di Bonian! Dapatkan Bonus Point '+numeral($(this).data('point')).format('0,0')+' serta dapatkan Potongan Kupon dan Voucher Diskon, Beli Sekarang ! ','_blank','width=600, height=368');
         return false;
     });
     $('.igShare').on('click',function(){
@@ -680,21 +680,21 @@ $(document).ready(function() {
         // return false;
     });
     $('.smsShare').on('click',function(){
-        window.open('sms:;?&body=Beli '+$(this).data('title')+' '+$(this).data('url')+'/'+reff+'/sms Harga Promo Rp. '+numeral($(this).data('price')).format('0,0')+' hanya di Bonian! Dapatkan Bonus Point '+numeral($(this).data('point')).format('0,0')+' serta dapatkan Potongan Kupon dan Voucher Diskon, Beli Sekarang ! ','_self','width=600, height=368')
+        window.open('sms:;?&body=Beli '+$(this).data('title')+' '+$(this).data('url')+'/sms/'+reff+' Harga Promo Rp. '+numeral($(this).data('price')).format('0,0')+' hanya di Bonian! Dapatkan Bonus Point '+numeral($(this).data('point')).format('0,0')+' serta dapatkan Potongan Kupon dan Voucher Diskon, Beli Sekarang ! ','_self','width=600, height=368')
         return false;
     });
 
     $('.fbShare').on('click',function(){
-        window.open('https://www.facebook.com/sharer.php?u='+$(this).data('url')+'/'+reff+'/fb','_blank','width=600, height=368');
+        window.open('https://www.facebook.com/sharer.php?u='+$(this).data('url')+'/fb/'+reff+'','_blank','width=600, height=368');
         return false;
     });
 
     $('.lineShare').on('click',function(){
-        window.open('https://lineit.line.me/share/ui?url='+$(this).data('url')+'/'+reff+'/line&text=Beli '+$(this).data('title')+' Harga Promo Rp. '+numeral($(this).data('price')).format('0,0')+' hanya di Bonian! Dapatkan Bonus Point '+numeral($(this).data('point')).format('0,0')+' serta dapatkan Potongan Kupon dan Voucher Diskon, Beli Sekarang ! ','_blank','width=600, height=368');
+        window.open('https://lineit.line.me/share/ui?url='+$(this).data('url')+'/ln/'+reff+'&text=Beli '+$(this).data('title')+' Harga Promo Rp. '+numeral($(this).data('price')).format('0,0')+' hanya di Bonian! Dapatkan Bonus Point '+numeral($(this).data('point')).format('0,0')+' serta dapatkan Potongan Kupon dan Voucher Diskon, Beli Sekarang ! ','_blank','width=600, height=368');
         return false;
     });
     $('.twitterShare').on('click',function(){
-        window.open('https://twitter.com/share?url='+$(this).data('url')+'/'+reff+'/twitter&text=Beli '+$(this).data('title')+' Harga Promo Rp. '+numeral($(this).data('price')).format('0,0')+' hanya di Bonian! Dapatkan Bonus Point '+numeral($(this).data('point')).format('0,0')+' serta dapatkan Potongan Kupon dan Voucher Diskon, Beli Sekarang ! ','_blank','width=600, height=368');
+        window.open('https://twitter.com/share?url='+$(this).data('url')+'/tw/'+reff+'&text=Beli '+$(this).data('title')+' Harga Promo Rp. '+numeral($(this).data('price')).format('0,0')+' hanya di Bonian! Dapatkan Bonus Point '+numeral($(this).data('point')).format('0,0')+' serta dapatkan Potongan Kupon dan Voucher Diskon, Beli Sekarang ! ','_blank','width=600, height=368');
         return false;
     });
 });
